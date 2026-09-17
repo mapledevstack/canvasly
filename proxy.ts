b332e44518d6b200b3e8fa81bd1c8005513cdf65
maple <maple.devstack@gmail.com>
2026-09-18 01:13:28 +0530

    refactor: format and improve structure of clerkMiddleware implementation in proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,24 @@
 import { clerkMiddleware } from "@clerk/nextjs/server"
 
-export default clerkMiddleware(async (auth, req) => {
-  const { pathname } = req.nextUrl
+export default clerkMiddleware(
+  async (auth, req) => {
+    const { pathname } = req.nextUrl
 
-  if (pathname !== "/") {
-    await auth.protect()
+    if (pathname !== "/") {
+      await auth.protect()
+    }
+  },
+  {
+    frontendApiProxy: {
+      enabled: true,
+    },
   }
-})
+)
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for Clerk's auto-proxy path
     "/__clerk/:path*",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 }
