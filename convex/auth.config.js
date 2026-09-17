e13425a76f255fe90ae080669d7972db5b7e6ff1
maple <maple.devstack@gmail.com>
2026-09-18 02:12:27 +0530

    refactor: update authentication domain configuration based on environment

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,11 @@
+const isProduction = process.env.NODE_ENV === "production"
+
 export default {
   providers: [
     {
-      domain: "https://popular-grub-6856.clerk.accounts.dev",
+      domain: isProduction
+        ? "https://clerk.canvas.snowfluff.online"
+        : "https://popular-grub-6856.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
