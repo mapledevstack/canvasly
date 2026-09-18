981b3c73ce53817cf4fef44b54529959a33f4483
maple <maple.devstack@gmail.com>
2026-09-18 18:34:00 +0530

    refactor: update domain configuration to use environment variable for Clerk issuer URL

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,11 +1,7 @@
-const isProduction = process.env.NODE_ENV === "production"
-
 export default {
   providers: [
     {
-      domain: isProduction
-        ? "https://clerk.canvas.snowfluff.online"
-        : "https://popular-grub-6856.clerk.accounts.dev",
+      domain: process.env.CLERK_ISSUER_URL,
       applicationID: "convex",
     },
   ],
